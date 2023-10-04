433768bf17f66ee214dca19430d0d846524de25a
Theo Last <finchasaurus@gmail.com>
2023-10-04 15:11:15 -0600

    added readme and example

--- examples/Example.tsx
+++ examples/Example.tsx
@@ -1,15 +1,7 @@
-
-# Catppuccin Roact
-
-Roact hooks and context to use with the catppuccin theme.
-## Usage/Examples
-
-``` tsx
 import Roact from "@rbxts/roact";
-import { useTheme } from "path-to/ThemeContext";
+import { useTheme } from "../src/ThemeContext";
 
 export default function Example() {
 	const theme = useTheme();
 	return <frame BackgroundColor3={theme.Theme.Base} Size={UDim2.fromScale(1, 1)} />;
 }
-```
